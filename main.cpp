e5cfd47cf8b308c0575824ba354a024d83c74239
Dillon <ninjamills@hotmail.com>
2017-11-15 10:20:47 -0900

    Added main.cpp and structs.hpp
    getting things set up.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// main.cpp
+//
+// CS201_Project
+// Videogame
+//
+// Team:
+// Grace Bolt
+// Liam Eberhart
+// Dillon Mills
+//
+// Initializes game.
+
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ structs.hpp
@@ -0,0 +1,21 @@
+// structs.hpp
+//
+// CS201_Project
+// Videogame
+//
+// Team:
+// Grace Bolt
+// Liam Eberhart
+// Dillon Mills
+//
+// contains important structs
+
+struct Player{
+    double health;
+    //...
+};
+
+struct Enemy{
+    double health;
+    //...
+};
