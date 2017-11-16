8036431b7ac88fb9ba5d2e519da61da6057cc153
LiamE <leberhart@alaska.edu>
2017-11-15 17:47:43 -0900

    added a dummy .cpp for testing

---
+++ makeMap.cpp
@@ -0,0 +1,18 @@
+//makeMap.cpp
+//
+// CS201_Project
+// Videogame
+//
+// Team:
+// Grace Bolt
+// Liam Eberhart
+// Dillon Mills
+//
+// creates the map, so far just a single room
+
+#include "structs.hpp"
+
+void blarg()
+{
+	//This is mostly just a test to see if it works
+}
