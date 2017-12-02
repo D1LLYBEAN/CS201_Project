f71377ea4963aab6e95c70cff6900a51ea8cd1f7
Dillon <ninjamills@hotmail.com>
2017-12-02 13:41:08 -0900

    It didn't work, but it didn't break.

--- makeMap.hpp
+++ makeMap.hpp
@@ -21,3 +21,4 @@ void showRoom(vector<vector<unsigned char>> printRoom);
 void generateRoom();
 void printFloor(Floor f);
 void makeFloor(Position entr, Position exit, Floor & f);
+void nextRoom(int dir);
